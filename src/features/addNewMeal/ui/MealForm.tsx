7acf7318410d561a0be21f50fd7cdc1efadf2157
Sergei Ivanov <38918636+sergeiivanow@users.noreply.github.com>
2023-04-24 23:44:19 +0300

    Add some logic

--- src/features/addNewMeal/ui/MealForm.tsx
+++ src/features/addNewMeal/ui/MealForm.tsx
@@ -1,39 +1,47 @@
 import React, {useState, useRef} from 'react'
 import {StyleSheet, View} from 'react-native'
 import * as UI from 'shared/ui'
-// import {useNavigationActions} from 'shared/lib'
-// import {useMeal} from 'entities/meal'
+import {useNavigationActions} from 'shared/lib'
+import {useMeal} from 'entities/meal'
 import {useStyles} from 'shared/theme'
 import PagerView from 'react-native-pager-view'
 
 export function MealForm() {
   const pagerViewRef = useRef<PagerView>(null)
   const [currentPage, setCurrentPage] = useState(0)
-  // const {goBack} = useNavigationActions()
-  const [value, setValue] = useState('')
-  const [value1, setValue1] = useState('')
-  // const {addNewMeal} = useMeal()
+  const {goBack} = useNavigationActions()
+  const [ate, setAte] = useState('')
+  const [drank, setDrank] = useState('')
+  const [contect, setContect] = useState('')
+  const {addNewMeal} = useMeal()
   const {styles} = useStyles(createStyles)
+  const saveActive = ate && drank && contect
 
-  // function save() {
-  //   addNewMeal({
-  //     mealDate: new Date().toISOString(),
-  //     ate: value,
-  //     drank: 'Апельсиновый сок',
-  //     contect: 'Забежал в столовку',
-  //     evaluation: 'От 5 до 7',
-  //     volume: '5 кулаков',
-  //   })
-  //   goBack()
-  // }
+  function save() {
+    addNewMeal({
+      mealDate: new Date().toISOString(),
+      ate,
+      drank,
+      contect,
+      evaluation: 'От 5 до 7',
+      volume: '5 кулаков',
+    })
+    goBack()
+  }
 
   function scrollToPage(page: number) {
-    return () => {
-      if (pagerViewRef.current) {
-        pagerViewRef.current.setPage(page)
-        setCurrentPage(page)
-      }
+    if (pagerViewRef.current) {
+      pagerViewRef.current.setPage(page)
+      setCurrentPage(page)
     }
+  }
+
+  function previous() {
+    scrollToPage(currentPage - 1)
+  }
+
+  function next() {
+    scrollToPage(currentPage + 1)
   }
 
   return (
@@ -44,14 +52,19 @@ export function MealForm() {
         initialPage={currentPage}
         scrollEnabled={false}
         onPageSelected={event => setCurrentPage(event.nativeEvent.position)}>
-        <View key="1">
-          <UI.TextInput value={value} onChangeText={setValue} />
+        <View key="ate">
+          <UI.TextInput value={ate} onChangeText={setAte} />
         </View>
-        <View key="2">
-          <UI.TextInput value={value1} onChangeText={setValue1} />
+        <View key="drank">
+          <UI.TextInput value={drank} onChangeText={setDrank} />
+        </View>
+        <View key="contect">
+          <UI.TextInput value={contect} onChangeText={setContect} />
         </View>
       </PagerView>
-      <UI.TextButton title="Save" onPress={scrollToPage(1)} />
+      <UI.TextButton title="Назад" onPress={previous} />
+      <UI.TextButton title="Дальше" onPress={next} />
+      {saveActive && <UI.TextButton title="Сохранить" onPress={save} />}
     </>
   )
 }
